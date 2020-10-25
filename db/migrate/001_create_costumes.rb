69a33f6330e9874075a861a316ee5a7faa50140f
Jung Lee <lee.jung.s95@gmail.com>
2020-10-25 20:38:40 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -6,8 +6,8 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
             t.float :price
             t.string :size
             t.string :image_url
-            t.datetime :created_at
-            t.datetime :updated_at
+            t.boolean :created_at
+            t.boolean :updated_at
         end
     end
 end
